0ad7222e12a5162c8a7f6eb200cc7be2eec7d223
Cheryl Qian <cq42@cornell.edu>
2025-04-24 16:29:04 -0400

    simple user input

--- todo.py
+++ todo.py
@@ -1,7 +1,15 @@
-Enter a task (or type 'done' to finish): Buy groceries  
-Enter a task (or type 'done' to finish): Call Mom  
-Enter a task (or type 'done' to finish): done
+# Enter a task (or type 'done' to finish): Buy groceries  
+# Enter a task (or type 'done' to finish): Call Mom  
+# Enter a task (or type 'done' to finish): done
 
-Your To-Do List:
-1.⁠ ⁠Buy groceries  
-2.⁠ ⁠Call Mom
+# Your To-Do List:
+# 1.⁠ ⁠Buy groceries  
+# 2.⁠ ⁠Call Mom
+
+
+todo = "Your To-Do List"
+i = 1
+
+
+x = input("Enter a task (or type 'done' to finish): ")
+
