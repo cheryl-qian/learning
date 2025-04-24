b769e2096ac8ec1837b14b6a677fb71977de3efe
Cheryl Qian <cq42@cornell.edu>
2025-04-24 16:28:09 -0400

    adding todo boilerplate

---
+++ todo.py
@@ -0,0 +1,7 @@
+Enter a task (or type 'done' to finish): Buy groceries  
+Enter a task (or type 'done' to finish): Call Mom  
+Enter a task (or type 'done' to finish): done
+
+Your To-Do List:
+1.⁠ ⁠Buy groceries  
+2.⁠ ⁠Call Mom
